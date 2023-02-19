cef233ec8be6dd51d0b56be3993672679ae260a4
Ishu Singh <ishu.111636@gmail.com>
2023-02-20 02:01:20 +0530

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export const Home  = () => {
   return (
-    <div style={{"text-align":"center"}}> To be created ... </div>
+    <div style={{"text-align":"center"}}> 
+    <div>
+    To be created ... 
+    </div>
+    </div>
   )
 }
